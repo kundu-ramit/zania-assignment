83070e4eaa80d79078816f6d23e66085c738dd29
kundu-ramit123 <140112358+kundu-ramit123@users.noreply.github.com>
2024-04-26 20:17:17 +0530

    .

---
+++ frontend/src/components/Card/ImageCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, Typography } from '@mui/material';
+import './ImageCard.css'
+
+const ImageCard = ({ imageUrl, title, description }) => {
+    return (
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={imageUrl}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  };
+  
+  export default ImageCard;
---
+++ frontend/src/containers/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ImageCard from '../../components/Card/ImageCard'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <ImageCard imageUrl={} title={} description={}/>
+    )
+  }
+}
